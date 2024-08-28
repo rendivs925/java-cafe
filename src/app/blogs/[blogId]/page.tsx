4f18a9f6660aac4266030c3c60a155656e065a5c
rendi12345678 <hardleberg@gmail.com>
2024-08-28 15:31:22 +0700

    fix

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -2,16 +2,21 @@ import { getBlogByIdAction } from "@/actions/getBlogByIdAction";
 import BaseContainer from "@/components/BaseContainer";
 import BaseContent from "@/components/BaseContent";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm"; // Import remark-gfm plugin for GitHub Flavored Markdown
 
-export default async function page({ params }: { params: { blogId: string } }) {
+export default async function Page({ params }: { params: { blogId: string } }) {
   const { blogId } = params;
   const response = await getBlogByIdAction(blogId);
-  const content = response.data?.content;
+  const content = response.data?.content || "";
 
   return (
     <BaseContainer>
       <BaseContent className="max-w-[80ch] mx-auto">
-        <ReactMarkdown>{content}</ReactMarkdown>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]} // Use remark-gfm for GitHub Flavored Markdown
+        >
+          {content}
+        </ReactMarkdown>
       </BaseContent>
     </BaseContainer>
   );
