28ced25975aeb39c1114e2f585872a62868dbe88
rendi12345678 <hardleberg@gmail.com>
2024-11-03 01:58:46 +0700

    fix scroll jumping

--- src/components/ProductCardButton.tsx
+++ src/components/ProductCardButton.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react"
+import { addProductToCartAction } from "@/actions/addProductToCartAction";
 import { Button } from "./ui/button";
 import useAppContext from "@/hooks/useAppContext";
 
@@ -21,7 +22,32 @@ function ProductCardButton({
   stock,
   weight,
 }: ProductCardButtonProps) {
-  const { addProductToCart } = useAppContext();
+  const { user, setTotalItems } = useAppContext();
+
+  const addProductToCart = async (product: {
+    productId: string;
+    profit: number;
+    title: string;
+    imgUrl: string;
+    price: number;
+    stock: number;
+    weight: number;
+  }) => {
+    try {
+      const data = await addProductToCartAction({
+        userId: user._id,
+        products: [product],
+      });
+
+      setTotalItems((data?.totalItems as number) || 1);
+
+      // if (data?.status === "success") {
+      //   toast({ description: data.message });
+      // }
+    } catch (error) {
+      console.error("Failed to add product to cart:", error);
+    }
+  };
 
   const handleAddToCartClick = () => {
     addProductToCart({
