a096bdb449b6759d73c96b00866aed415e92c64d
rendi12345678 <hardleberg@gmail.com>
2024-11-03 01:45:17 +0700

    fix 1

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,10 +2,9 @@ import ProductCatalog from "@/components/ProductCatalog";
 import BaseContainer from "@/components/BaseContainer";
 import BaseHeader from "@/components/BaseHeader";
 import BaseContent from "@/components/BaseContent";
-import { checkAndUpdateExpiredOrders } from "@/actions/checkAndUpdateExpiredOrders";
+// import { checkAndUpdateExpiredOrders } from "@/actions/checkAndUpdateExpiredOrders";
 
 export default async function Products() {
-  await checkAndUpdateExpiredOrders();
 
   return (
     <BaseContainer>
